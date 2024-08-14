9e4674ed8f0710a1d8a7a49edeb63e26df9436f9
gaixixon <gaixixon@gmail.com>
2024-08-14 14:13:40 +0700

    Updated on 2024-08-14_14:13:40

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~/gaixixon.github.io
 #sed '1 s/^.*$/Updated on $(date +%Y-%m-%d_%H:%M:%S)/' iptv
-python3 ~/tv360/gaixixon.github.io/getlink.py
+python3 getlink.py
 #sleep 15
 git add .
 git commit -m "Updated on $(date +%Y-%m-%d_%H:%M:%S)"
