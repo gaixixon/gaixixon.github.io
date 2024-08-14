9f8ff8c2291622ce1ad900f99030bdd9422a758b
gaixixon <gaixixon@gmail.com>
2024-08-14 04:00:50 +0000

    Update on $(date +"%Y-%m-%d")

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~/tv360/gaixixon.github.io
 python3 ~/tv360/gaixixon.github.io/getlink.py
-sleep 15
+#sleep 15
 git add .
-git commit -m '`date` aaaaaaaaaaaa'
+git commit -m 'Update on $(date +"%Y-%m-%d")'
 git push
