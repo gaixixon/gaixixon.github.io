b1768d09fc9446d60b942ef043697a36297463fb
EC2 Default User <ec2-user@ip-172-31-39-175.ap-southeast-2.compute.internal>
2024-08-15 23:26:17 +0700

    Updated on 2024-08-15_23:26:17

--- autoupdate.sh
+++ autoupdate.sh
@@ -4,7 +4,6 @@ timestamp=$(date +%Y-%m-%d_%H:%M:%S)
 sed -i -e "s/#--- Updated .*---/#--- Updated ${timestamp}  ---/g" iptv
 sed -i -e "s/Updated on .*!/Updated on ${timestamp}!/g" iptv
 python3 getlink.py
-sleep 15
 git add .
 git commit -m "Updated on $(date +%Y-%m-%d_%H:%M:%S)"
 git push
