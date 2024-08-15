55ecc54b2897e56f70b891117f1c4cd8a59c7ffb
EC2 Default User <ec2-user@ip-172-31-39-175.ap-southeast-2.compute.internal>
2024-08-15 23:22:28 +0700

    Updated on 2024-08-15_23:22:28

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 cd ~/gaixixon.github.io
 timestamp=$(date +%Y-%m-%d_%H:%M:%S)
-sed -i -e 's/#--- Updated .*---/#--- Updated ${date +%Y-%m-%d_%H:%M:%S} ---/' iptv
+sed -i -e "s/#--- Updated .*---/#--- Updated ${timestamp}  ---/g" iptv
+sed -i -e "s/Updated on .*!/Updated on ${timestamp}!/g" iptv
 python3 getlink.py
-#sleep 15
+sleep 15
 git add .
 git commit -m "Updated on $(date +%Y-%m-%d_%H:%M:%S)"
 git push
