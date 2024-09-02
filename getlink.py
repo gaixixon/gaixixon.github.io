79e8f07136b1ce92abfbc9c066547e43acd899eb
EC2 Default User <ec2-user@ip-172-31-19-199.ap-southeast-1.compute.internal>
2024-09-02 22:47:29 +0700

    Updated on 2024-09-02_22:47:29

--- getlink.py
+++ getlink.py
@@ -54,7 +54,7 @@ def gettv(channel , link):
     driver.get(link)
     i=0
     while i<10:
-        i++
+        i+=1
         # Call the function to get GET requests
         m3u8_requests = get_m3u8_requests()
 
