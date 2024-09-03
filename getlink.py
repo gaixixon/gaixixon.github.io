2a8bddf21a3fd29535f65ce5dc6bdb9612d56998
EC2 Default User <ec2-user@ip-172-31-19-199.ap-southeast-1.compute.internal>
2024-09-04 06:56:46 +0700

    add pause

--- getlink.py
+++ getlink.py
@@ -51,6 +51,7 @@ def get_m3u8_requests():
 def gettv(channel , link):
     try:
         print('start to get tv channel ' + channel)
+        time.sleep(random.randint(1, 3)) 
         global tv_link
         driver.get(link)
         i=0
@@ -79,9 +80,10 @@ def gettv(channel , link):
 
 for url in urls:
     gettv(url["channel"] , url["tvurl"])
-    time.sleep(random.randint(1, 15))    #sleep to avoid bot detect
+    time.sleep(random.randint(1, 5))    #sleep to avoid bot detect
 
 driver.quit()
 
 with open('iptv.json','w') as f:
     f.write('[' + tv_link + '{"channel":"test", "link":"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"}]')
+    f.close()
