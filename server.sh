07cc883c1eb0374b80816c757cf14c0f35b6eea3
EC2 Default User <ec2-user@ip-172-31-19-199.ap-southeast-1.compute.internal>
2024-09-07 18:32:25 +0700

    Updated on 2024-09-07  18:32:25

--- server.sh
+++ server.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-kill -15 $(ps aux | grep python3 | grep -v grep | awk '{print $2}')
+#kill -15 $(ps aux | grep python3 | grep -v grep | awk '{print $2}')
+
+#ps aux | grep python3 | grep -v grep | awk '{print $2}' | while read pid; do
+#    kill -15 "$pid"
+#done
+
+
+ps aux | grep python3 | grep -v grep | awk '{print $2}' | xargs kill -15
+
 cd ~/iptv
-python3 iptv.py -u 2>/tmp/http_request.log &
+python3 iptv.py -u 2>>/tmp/http_request.log &
 
