a354fd98647c8b719db3e3ce3ec8595db1483a09
EC2 Default User <ec2-user@ip-172-31-19-199.ap-southeast-1.compute.internal>
2024-09-29 21:10:04 +0700

    dhdj

--- server.sh
+++ server.sh
@@ -6,8 +6,10 @@
 #done
 
 
-ps aux | grep python3 | grep -v grep | awk '{print $2}' | xargs kill -15
+sudo ps aux | grep python3 | grep -v grep | awk '{print $2}' | xargs kill -15
 
-cd ~/iptv
+#cd ~/iptv
 #python3 iptv.py -u 2>>/tmp/http_request.log &
-python3 iptv.py -u 2>>/dev/null &
+sudo python3 /home/ec-user/https/server.py -u 2>>/tmp/https.log
+
+sudo python3 /home/ec2-user/iptv/iptv.py -u 2>>/dev/null &
