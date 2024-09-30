f613a668d0a39ea88fe08f8e9d5f893dd59be4d1
EC2 Default User <ec2-user@ip-172-31-19-199.ap-southeast-1.compute.internal>
2024-09-30 13:13:53 +0700

    Updated on 2024-09-30  13:13:53

--- server.sh
+++ server.sh
@@ -10,6 +10,6 @@ sudo ps aux | grep python3 | grep -v grep | awk '{print $2}' | xargs kill -15
 
 #cd ~/iptv
 #python3 iptv.py -u 2>>/tmp/http_request.log &
-sudo python3 /home/ec2-user/https/server.py -u 2>>/tmp/https.log
+sudo nohup python3 /home/ec2-user/https/server.py -u 2>>/tmp/https.log &
 
-sudo python3 /home/ec2-user/iptv/iptv.py -u 2>>/dev/null &
+sudo nohup python3 /home/ec2-user/iptv/iptv.py -u 2>>/dev/null &
